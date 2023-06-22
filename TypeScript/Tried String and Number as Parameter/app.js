7393a70f9e10547f7b34712b09be1585d4033033
raghulashok16 <114047327+raghulashok16@users.noreply.github.com>
2023-06-22 14:07:20 +0530

    updated file

---
+++ TypeScript/Tried String and Number as Parameter/app.js
@@ -0,0 +1,15 @@
+"use strict";
+// function add (n1: number, n2: number){
+//     return n1+n2;
+// }
+// const number1 ='5';
+// const number2 = 2.2;
+// console.log(add(number1,number2)); 
+// //Argument of type 'string' is not assignable to parameter of type 'number'.ts(2345)
+// function add(n1: number, n2: number) {
+//     return n1 + n2;
+// }
+// const number1 = '5';
+// const number2 = 2.2;
+// console.log(add(+number1, number2));
+// // need to use unary operator to convert from string to number type.
