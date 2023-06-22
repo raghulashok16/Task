1812f9f1fad1bcde1a607a78834bd6726499b5cb
raghulashok16 <114047327+raghulashok16@users.noreply.github.com>
2023-06-22 14:25:53 +0530

    changed

--- TypeScript/Basic Setup/app.js
+++ TypeScript/Basic Setup/app.js
@@ -1,15 +1,15 @@
 "use strict";
-// function add (n1: number, n2: number){
-//     return n1+n2;
-// }
-// const number1 ='5';
-// const number2 = 2.2;
-// console.log(add(number1,number2)); 
-// //Argument of type 'string' is not assignable to parameter of type 'number'.ts(2345)
-// function add(n1: number, n2: number) {
-//     return n1 + n2;
-// }
-// const number1 = '5';
-// const number2 = 2.2;
-// console.log(add(+number1, number2));
-// // need to use unary operator to convert from string to number type.
+function add(n1, n2, per) {
+    if (per) {
+        // console.log("Result : " + n1 + n2);//mathematical error-Type Coercion
+        let total = n1 + n2;
+        console.log("Result : " + total); //correct way without error
+    }
+    else {
+        console.log("You Dont Have Permission");
+    }
+}
+const number1 = 5;
+const number2 = 2.2;
+const permission = true;
+add(number1, number2, permission);
