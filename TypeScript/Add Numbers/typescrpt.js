0e95894dabcc52c74a825e384875745d510ff86e
raghulashok16 <114047327+raghulashok16@users.noreply.github.com>
2023-06-22 08:03:51 +0530

    typescript first task added

---
+++ TypeScript/Add Numbers/typescrpt.js
@@ -0,0 +1,5 @@
+var num = 1;
+function add(num) {
+    return num + num;
+}
+console.log(add(2));
---
+++ TypeScript/Add Numbers/typescrpt.ts
@@ -0,0 +1,5 @@
+const num: number = 1;
+function add(num: number): number {
+    return num + num;
+}
+console.log(add(2));
