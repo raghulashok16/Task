6e6118ef5c965c9c91a0bc1111f1a1f23ed4beb2
raghulashok16 <114047327+raghulashok16@users.noreply.github.com>
2023-06-22 16:23:27 +0530

    array and tuple added

---
+++ TypeScript/5.Tried any,unknown,never/app.js
@@ -0,0 +1,11 @@
+"use strict";
+const val1 = 5;
+const val2 = 2.2;
+// const val3: never = true //w cannot assign to thye never
+// let y: undefined = 2;//error//only undefined allowed
+let z = null;
+console.log(val1);
+console.log(val2);
+// console.log(val3);
+// console.log(y);
+console.log(z);
---
+++ TypeScript/5.Tried any,unknown,never/app.ts
@@ -0,0 +1,14 @@
+
+const val1: any = 5;
+const val2: unknown = 2.2;
+// const val3: never = true //w cannot assign to thye never
+// let y: undefined = 2;//error//only undefined allowed
+
+let z: null = null;
+
+console.log(val1);
+console.log(val2);
+// console.log(val3);
+// console.log(y);
+console.log(z);
+
